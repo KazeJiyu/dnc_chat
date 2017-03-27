349fc5fd3b1fa5333f3a0cb7866bc43320aeb88a
Emmanuel CHEBBI <emmanuel.chebbi@outlook.fr>
2017-03-27 13:55:00 +0200

    Implement various DNC commands

--- server/dnc/_commands.py
+++ server/dnc/_commands.py
@@ -1,6 +1,8 @@
 # Standard modules
 
 # Custom utility functions
+from server.dnc._data import ConnectionStatus, Client
+from server.utils.errors import abort_if
 from server.utils.patterns import Dispatcher
 
 # Restrict "from _commands import *"
@@ -18,3 +20,174 @@ class CommandDispatcher(Dispatcher):
         
         handler = self.dispatch(cmd.upper(), lambda *_: "298 ERR_MALFORMEDREQUEST")
         return handler(connection, args)
+
+@CommandDispatcher.register_cmd
+def connect(connection, args):
+    abort_if(lambda: connection.status == ConnectionStatus.CONNECTED, "200 ERR_ALREADYCONNECTED")
+    abort_if(lambda: len(args) < 1, "203 ERR_NOTENOUGHARGS")
+    abort_if(lambda: args[0] in connection._protocol.clients, "205 ERR_NICKNAMEINUSE")
+
+    pseudo = args[0]
+    
+    if len(pseudo) > 10 or pseudo.startswith('@') or ',' in pseudo:
+        return "206 ERR_INVALIDNICKNAME"
+    
+    connection.status = ConnectionStatus.CONNECTED    
+    connection.client = Client(pseudo, connection)
+    connection._protocol.clients[pseudo] = connection.client
+    return "100 RPL_DONE"
+
+@CommandDispatcher.register_cmd
+def quit(connection, args):  # @ReservedAssignment
+    abort_if(lambda: connection.status == ConnectionStatus.NOT_CONNECTED, "201 ERR_NOTCONNECTED")
+
+    connection.write_all(":" + connection.client.pseudo + " QUIT " + " ".join(args))
+        
+    connection.close();
+        
+    # no reply will be sent
+    return None
+
+@CommandDispatcher.register_cmd
+def message(connection, args):
+    abort_if(lambda: connection.status == ConnectionStatus.NOT_CONNECTED, "201 ERR_NOTCONNECTED")
+    abort_if(lambda: connection.status == ConnectionStatus.AWAY, "-202 ERR_BADSTATUS")
+    abort_if(lambda: len(args) < 1, "203 ERR_NOTENOUGHARGS")
+        
+    if args[0].startswith('!'):                
+        return connection._protocol.chat_bot.react(*args)
+
+    connection.write_all(":" + connection.client.pseudo + " MESSAGE " + " ".join(args))
+            
+    return "100 RPL_DONE"
+
+@CommandDispatcher.register_cmd
+def whisper(connection, args):
+    abort_if(lambda: connection.status == ConnectionStatus.NOT_CONNECTED, "201 ERR_NOTCONNECTED")
+    abort_if(lambda: connection.status == ConnectionStatus.AWAY, "-202 ERR_BADSTATUS")
+    abort_if(lambda: len(args) < 2, "203 ERR_NOTENOUGHARGS")
+    
+    dest = args[0]
+    message = " ".join(args[1:])
+    
+    try:
+        connection.write_to(dest, ":" + connection.client.pseudo + " WHISPER " + message)
+    except KeyError:
+        return "204 ERR_NICKNAMENOTEXIST"
+    
+    return "100 RPL_DONE"
+
+@CommandDispatcher.register_cmd
+def ask_whisper(connection, args):
+    abort_if(lambda: connection.status == ConnectionStatus.NOT_CONNECTED, "201 ERR_NOTCONNECTED")
+    abort_if(lambda: connection.status == ConnectionStatus.AWAY, "-202 ERR_BADSTATUS")
+    abort_if(lambda: len(args) < 2, "203 ERR_NOTENOUGHARGS")
+    
+    dest = args[0]
+    
+    try:
+        connection.write_to(dest, ":" + connection.client.pseudo + " ASK_WHISPER")
+    except KeyError:
+        return "204 ERR_NICKNAMENOTEXIST"
+    
+    return "100 RPL_DONE"
+
+@CommandDispatcher.register_cmd
+def reply_whisper(connection, args):
+    abort_if(lambda: connection.status == ConnectionStatus.NOT_CONNECTED, "201 ERR_NOTCONNECTED")
+    abort_if(lambda: connection.status == ConnectionStatus.AWAY, "-202 ERR_BADSTATUS")
+    abort_if(lambda: len(args) < 2, "203 ERR_NOTENOUGHARGS")
+    
+    dest = args[0]
+    message = " ".join(args[1:])
+    
+    try:
+        connection.write_to(dest, ":" + connection.client.pseudo + " REPLY_WHISPER " + message)
+    except KeyError:
+        return "204 ERR_NICKNAMENOTEXIST"
+    
+    return "100 RPL_DONE"
+
+@CommandDispatcher.register_cmd
+def file(connection, args):
+    abort_if(lambda: connection.status == ConnectionStatus.NOT_CONNECTED, "201 ERR_NOTCONNECTED")
+    abort_if(lambda: len(args) < 3, "203 ERR_NOTENOUGHARGS")
+    
+    dest, file, size = args[0], args[1], args[2]
+    
+    try:
+        connection.write_to(dest, ":" + connection.client.pseudo + " FILE " + file + " " + size)
+    except KeyError:
+        return "204 ERR_NICKNAMENOTEXIST"
+    
+    return "100 RPL_DONE"
+
+@CommandDispatcher.register_cmd
+def mute(connection, args):
+    abort_if(lambda: connection.status == ConnectionStatus.NOT_CONNECTED, "201 ERR_NOTCONNECTED")
+    abort_if(lambda: len(args) < 1, "203 ERR_NOTENOUGHARGS")
+    
+    in_error = []
+    
+    for pseudo in args:
+        try:
+            to_mute = connection._protocol.clients[pseudo]
+        except KeyError:
+            in_error.append(pseudo)
+        else:
+            connection.ignored.append(to_mute.connection)
+        
+    return "100 RPL_DONE" if not in_error else "204 ERR_NICKNAMENOTEXIST " + " ".join(in_error) 
+    
+@CommandDispatcher.register_cmd
+def listen(connection, args):
+    abort_if(lambda: connection.status == ConnectionStatus.NOT_CONNECTED, "201 ERR_NOTCONNECTED")
+    abort_if(lambda: len(args) < 1, "203 ERR_NOTENOUGHARGS")
+    
+    in_error = []
+    
+    for pseudo in args:
+        try:
+            to_listen = connection._protocol.clients[pseudo]
+        except KeyError:
+            in_error.append(pseudo)
+        else:
+            connection.ignored.remove(to_listen.connection)
+        
+    return "100 RPL_DONE" if not in_error else "204 ERR_NICKNAMENOTEXIST " + " ".join(in_error) 
+
+@CommandDispatcher.register_cmd
+def names(connection, args):
+    abort_if(lambda: connection.status == ConnectionStatus.NOT_CONNECTED, "201 ERR_NOTCONNECTED")
+    
+    return "101 RPL_NAMES " + " ".join(client for client in connection._protocol.clients)
+
+@CommandDispatcher.register_cmd
+def away(connection, args):
+    abort_if(lambda: connection.status == ConnectionStatus.NOT_CONNECTED, "201 ERR_NOTCONNECTED")
+    
+    connection.status = ConnectionStatus.AWAY
+    return "100 RPL_DONE"
+
+@CommandDispatcher.register_cmd
+def re(connection, args):
+    abort_if(lambda: connection.status == ConnectionStatus.NOT_CONNECTED, "201 ERR_NOTCONNECTED")
+    
+    connection.status = ConnectionStatus.CONNECTED
+    return "100 RPL_DONE"
+
+@CommandDispatcher.register_cmd
+def nick(connection, args):
+    abort_if(lambda: connection.status == ConnectionStatus.NOT_CONNECTED, "201 ERR_NOTCONNECTED")
+    abort_if(lambda: len(args) < 1, "203 ERR_NOTENOUGHARGS")
+    abort_if(lambda: args[0] in connection._protocol.clients, "205 ERR_NICKNAMEINUSE")
+    
+    new_nick = args[0]
+    
+    if len(new_nick) > 10 or new_nick.startswith('@') or ',' in new_nick:
+        return "206 ERR_INVALIDNICKNAME"
+    
+    connection.write_all(":" + connection.client.pseudo + " NICK " + " " + new_nick)
+    connection._protocol.clients.rename(connection.client, args[0])
+    
+    return "100 RPL_DONE"
