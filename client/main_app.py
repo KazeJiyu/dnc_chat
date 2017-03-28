54b4690053278630d9f313a467c4a9bfa8d3dd02
Emmanuel CHEBBI <emmanuel.chebbi@outlook.fr>
2017-03-28 16:01:41 +0200

    Make main_app.py executable from the command line
    The path toward other modules is directly added to python path within main_app.py

--- client/main_app.py
+++ client/main_app.py
@@ -1,4 +1,9 @@
 import sys
+import os
+
+# let python know other modules
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(os.path.dirname(__file__))
 
 from PyQt5.QtWidgets import QApplication
 from client.controllers.change_view import MainView
